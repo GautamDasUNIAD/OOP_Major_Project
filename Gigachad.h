468dbd02f4f06fd12000e5654d6a6aafd91058ec
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:27:33 +1030

    Added comments

--- Gigachad.h
+++ Gigachad.h
@@ -1,23 +1,27 @@
+// This is the header file for the "Gigachad" class 
+
 #ifndef GIGACHAD
 #define GIGACHAD
 
+// include relevant libraries
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
 #include "character.h"
 
+// derive gigachad class from character class
 class Gigachad : public character {
    public:
-    Gigachad();
-    void returnStats();
-    int choosemove();
+    Gigachad(); // constructor function that sets all the attributes to default values
+    void returnStats(); // function that prints out all the attributes of gigachad
+    int choosemove(); // function that randomly chooses a number between 1 and 3 return it to make a move
 
-    void attack();
-    void heal();
-    void boost();
+    void attack(); // function that calls the attackmove object to deal damage
+    void heal(); // function that calls the healmove object to heal health
+    void boost(); // function tha calls the statboostingmove object to heal health
 
-    ~Gigachad();
+    ~Gigachad(); // deconstructor function
 };
 
 #endif
