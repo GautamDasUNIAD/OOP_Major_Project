fd390607962693ac936829159f734d483f2384d5
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:22:31 +1030

    Update Gigachad.cpp

--- Gigachad.cpp
+++ Gigachad.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-#include <string>
+// This is the .cpp file for the "Gigachad" class 
+// include relevant libraries
+#include <iostream> // to print to the terminal 
+#include <string> 
 
+// Include relevant header files
 #include "Gigachad.h"
 #include "character.h"
 
+// constructor function
 Gigachad::Gigachad() {
     set_Name("Gigachad");
     std::string ascii_array[27] = {"⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿",
@@ -41,6 +45,7 @@ Gigachad::Gigachad() {
     set_critical(2);
 }
 
+// return stats
 void Gigachad::returnStats() {
     std::cout << " attack stat is: " << get_damage_factor() << std::endl;
     std::cout << " healing stat is: " << get_healing_factor() << std::endl;
@@ -49,11 +54,13 @@ void Gigachad::returnStats() {
     std::cout << " Current HP is: " << get_Current_HP() << std::endl;
 }
 
+// use srand to randomly choose the move for gigachad
 int Gigachad::choosemove() {
-    srand(time(0));
-    int value = rand() % 3 + 1;
+    srand(time(0)); // change the seed to allow for randomness
+    int value = rand() % 3 + 1; // get a random number between 3 and 1
     return value;
 }
+
 void Gigachad::attack() {}
 void Gigachad::heal() {}
 void Gigachad::boost() {}
