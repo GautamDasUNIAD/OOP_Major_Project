e6c5ad4199a5c1aef04203d0b296c6c288dd0964
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 11:12:30 +1030

    added comments

--- statboostingmove.h
+++ statboostingmove.h
@@ -1,25 +1,32 @@
+//this is the header file for the statboostingmove class
+
 #ifndef STATBOOSTINGMOVE
 #define STATBOOSTINGMOVE
 
+// include relevant libraries
 #include <iostream>
 #include <string>
 
+
+// include relevant header files
+#include "playable.h"
 #include "Gigachad.h"
 #include "Lazybones.h"
 #include "SleepyJeff.h"
 #include "Snorlax.h"
 #include "character.h"
 
+// create class
 class statboostingmove {
    private:
     int additional_damage_factor = 2;
     int additional_healing_factor = 2;
     int reduce_critical = 1;  // reduce chance of critical by
    public:
-    statboostingmove();
-    void boost_stats_player(playable* pptr);
-    void boost_stats_chad(Gigachad* gigaptr);
-    ~statboostingmove();
+    statboostingmove(); // constructor
+    void boost_stats_player(playable* pptr); //increase damage and haeling factor by 1, reduce critcal by 1 if it is 2 or above 
+    void boost_stats_chad(Gigachad* gigaptr); //increase damage and haeling factor by 1, reduce critcal by 1 if it is 2 or above
+    ~statboostingmove(); // deconstructor function
 };
 
 #endif
