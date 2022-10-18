8c91b600c6af019146ae547f5eaaec9097f80ba4
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:36:47 +1030

    added comments

--- SleepyJeff.h
+++ SleepyJeff.h
@@ -1,22 +1,26 @@
+// This is the header file for the "Lazybones" class 
 #ifndef SLEEPYJEFF
 #define SLEEPYJEFF
 
+// include relevant libraries
 #include <iostream>
 #include <string>
 
+// include relevant header files
 #include "character.h"
 #include "playable.h"
 
+// derive gigachad class from character class
 class SleepyJeff : public playable {
    public:
-    SleepyJeff();
-    void returnStats();
+    SleepyJeff(); // constructor function that sets all the attributes to default values
+    void returnStats(); // function that prints out all the attributes of the character
 
-    void attack();
-    void heal();
-    void boost();
+    void attack(); // function that calls the attackmove object to deal damage
+    void heal(); // function that calls the healmove object to heal health
+    void boost(); // function tha calls the statboostingmove object to heal health
 
-    ~SleepyJeff();
+    ~SleepyJeff(); // deconstructor function
 };
 
 #endif
