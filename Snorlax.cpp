6b22167daeff4065b35f6550457744c267408323
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:39:47 +1030

    added comments

--- Snorlax.cpp
+++ Snorlax.cpp
@@ -1,9 +1,13 @@
+// This is the .cpp file for the "Snorlax" class 
+// include relevant libraries
 #include <iostream>
 #include <string>
 
+// include relevant header files
 #include "Snorlax.h"
 #include "character.h"
 
+// constructor 
 Snorlax::Snorlax() {
     set_Name("Snorlax");
     std::string ascii_array[27] = {
@@ -69,6 +73,7 @@ Snorlax::Snorlax() {
     set_critical(10);
 }
 
+// return stats by printing out outputs for all attributes 
 void Snorlax::returnStats() {
     std::cout << " attack stat is: " << get_damage_factor() << std::endl;
     std::cout << " healing stat is: " << get_healing_factor() << std::endl;
@@ -76,7 +81,10 @@ void Snorlax::returnStats() {
     std::cout << " Maximum HP is: " << get_Max_HP() << std::endl;
     std::cout << " Current HP is: " << get_Current_HP() << std::endl;
 }
+
 void Snorlax::attack(){};
 void Snorlax::heal(){};
 void Snorlax::boost(){};
+
+// deconstructor function
 Snorlax::~Snorlax(){};
