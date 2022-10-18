cb7edcfcd710102cd75ab220bd51d47499b19a21
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:41:43 +1030

    Update Snorlax.h

--- Snorlax.h
+++ Snorlax.h
@@ -1,22 +1,26 @@
+// This is the header file for the "Snorlax" class 
 #ifndef SNORLAX
 #define SNORLAX
 
+// include relevant libraries
 #include <iostream>
 #include <string>
 
+// include relevant header files
 #include "character.h"
 #include "playable.h"
 
+// derive class from playable class
 class Snorlax : public playable {
    public:
-    Snorlax();
-    void returnStats();
+    Snorlax(); // constructor function that sets all the attributes to default values
+    void returnStats(); // function that prints out all the attributes of the character
 
-    void attack();
-    void heal();
-    void boost();
-
-    ~Snorlax();
+    void attack(); // function that calls the attackmove object to deal damage
+    void heal(); // function that calls the healmove object to heal health
+    void boost(); // function tha calls the statboostingmove object to heal health
+   
+    ~Snorlax(); // deconstructor function
 };
 
 #endif
