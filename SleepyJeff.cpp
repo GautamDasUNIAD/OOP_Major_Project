316cbe7a8e6ae0e0d406cdd0f1484d87432e879d
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2022-10-18 10:34:53 +1030

    added comments

--- SleepyJeff.cpp
+++ SleepyJeff.cpp
@@ -1,9 +1,13 @@
+// This is the .cpp file for the "SLeepyjeff" class 
+// include relevant libraries
 #include <iostream>
 #include <string>
 
+// include relevant header files
 #include "SleepyJeff.h"
 #include "character.h"
 
+// constructor function
 SleepyJeff::SleepyJeff() {
     set_Name("SleepyJeff");
     std::string ascii_array[27] = {
@@ -44,6 +48,7 @@ SleepyJeff::SleepyJeff() {
     set_critical(5);
 }
 
+// return stats by printing out outputs for all attributes 
 void SleepyJeff::returnStats() {
     std::cout << " attack stat is: " << get_damage_factor() << std::endl;
     std::cout << " healing stat is: " << get_healing_factor() << std::endl;
@@ -51,7 +56,10 @@ void SleepyJeff::returnStats() {
     std::cout << " Maximum HP is: " << get_Max_HP() << std::endl;
     std::cout << " Current HP is: " << get_Current_HP() << std::endl;
 }
+
 void SleepyJeff::attack(){};
 void SleepyJeff::heal(){};
 void SleepyJeff::boost(){};
+
+// deconstructor function
 SleepyJeff::~SleepyJeff(){};
